5f19596f3312a8ac269aeb476ab2be3b6597ef9c
Joseph Macgowan <jm524@st-andrews.ac.uk>
2024-04-05 12:20:31 +0100

    Added reponse code vlaidation and onSubmit

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -4,11 +4,40 @@ const ContactForm = ({}) => {
     const [firstName, setFirstName] = useState("")
     const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        const data = {
+            firstName,
+            lastName,
+            email
+        }
+        const url = "https:/127.0.0.1:5000/create_contact"
+        const options = {
+            method:"POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        const response = await fetch(url, options)
+        if (response.status !== 201 && response.status !== 201){
+            const message = await response.json()
+            alert(data.message)
+        } else {
+            
+        }
+    }
     
-    return <form>
+    return (<form onSubmit={onsubmit}>
         <div>
-            <label htmlFor="firstName">First Name: </label>
+        <label htmlFor="firstName">First Name: </label>
             <input type = "text" id = "firstName" value = {firstName} onChange={(e) => setFirstName(e.target.value)}/>
+        <label htmlFor="lastname">Last Name: </label>
+            <input type = "text" id = "lastName" value = {lastName} onChange={(e) => setLastName(e.target.value)}/>
+        <label htmlFor="email">First Name: </label>
+            <input type = "text" id = "email" value = {email} onChange={(e) => setEmail(e.target.value)}/>
         </div>
+        <button type="submit">Create Contact</button>
     </form>
-}
+)}
